e7102c2d8b7293ede03847ab9e5b4c112ab8beb1
Ziqi Wang <ziqi.ccc.wang@student.jyu.fi>
2021-10-22 21:59:37 +0300

    exercise 7.8

--- ultimate-hooks/src/App.js
+++ ultimate-hooks/src/App.js
@@ -1,4 +1,3 @@
-  
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
@@ -19,11 +18,17 @@ const useField = (type) => {
 
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
-
-  // ...
+  
+  useEffect(()=>{
+    axios.get(baseUrl).then(response=>{
+      setResources(response.data)
+    })
+  },[baseUrl])
 
   const create = (resource) => {
-    // ...
+    axios.post(baseUrl,resource).then(response=>{
+      setResources(resources.concat(response.data))
+    })
   }
 
   const service = {
